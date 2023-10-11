a12b9154863c555e772d9ff41c7bb8ba55a87d50
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-11 13:10:08 +0200

    articoli entity modified

--- EshopArticoli/src/main/java/com/eshop/eshop/entity/Articolo.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/entity/Articolo.java
@@ -2,14 +2,24 @@ package com.eshop.eshop.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Builder;
 
 @Entity
+@Builder
 public class Articolo {
 
     @Id
     private int articoloId;
     private String name;
-    private String description;
+    private String details;
     private double price;
 
+    public Articolo(){}
+
+    public Articolo(int articoloId, String name, String details, double price) {
+        this.articoloId = articoloId;
+        this.name = name;
+        this.details = details;
+        this.price = price;
+    }
 }
