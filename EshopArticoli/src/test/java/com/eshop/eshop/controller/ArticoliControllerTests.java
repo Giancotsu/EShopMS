d165beef6888968fe79a024c253c4e1d2f9238c1
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-11 12:14:18 +0200

    articoli controller tests class added

---
+++ EshopArticoli/src/test/java/com/eshop/eshop/controller/ArticoliControllerTests.java
@@ -0,0 +1,15 @@
+package com.eshop.eshop.controller;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ArticoliControllerTests {
+
+
+
+}
