04f3c1f3cddc2086f493c37c724902b023b5e470
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-13 14:20:14 +0200

    create item method created

--- EshopArticoli/src/main/java/com/eshop/eshop/service/impl/ItemServiceImpl.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/service/impl/ItemServiceImpl.java
@@ -1,11 +1,26 @@
 package com.eshop.eshop.service.impl;
 
 import com.eshop.eshop.dto.ItemDto;
+import com.eshop.eshop.dto.converter.ItemConverter;
+import com.eshop.eshop.models.ItemEntity;
+import com.eshop.eshop.repository.ItemRepository;
 import com.eshop.eshop.service.ItemService;
 
 public class ItemServiceImpl implements ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public ItemServiceImpl(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
     @Override
     public ItemDto createItem(ItemDto itemDto) {
-        return null;
+        //ItemEntity item = new ItemEntity(itemDto.getName(), itemDto.getDetails(), itemDto.getPrice());
+        ItemEntity item = ItemConverter.itemDtoToitem(itemDto);
+
+        ItemEntity newItem = itemRepository.save(item);
+
+        return ItemConverter.itemToItemDto(newItem);
     }
 }
