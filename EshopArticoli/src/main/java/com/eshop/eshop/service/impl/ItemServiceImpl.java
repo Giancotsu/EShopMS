001514e108a3a87e847470757d89413745532007
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-13 13:52:16 +0200

    item service implementation created

---
+++ EshopArticoli/src/main/java/com/eshop/eshop/service/impl/ItemServiceImpl.java
@@ -0,0 +1,11 @@
+package com.eshop.eshop.service.impl;
+
+import com.eshop.eshop.dto.ItemDto;
+import com.eshop.eshop.service.ItemService;
+
+public class ItemServiceImpl implements ItemService {
+    @Override
+    public ItemDto createItem(ItemDto itemDto) {
+        return null;
+    }
+}
