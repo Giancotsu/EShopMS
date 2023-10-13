12b5efad81e37ede870aa23682197f6a33841a59
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-13 14:19:28 +0200

    item repository enhanced

--- EshopArticoli/src/main/java/com/eshop/eshop/repository/ItemRepository.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/repository/ItemRepository.java
@@ -1,17 +1,18 @@
 package com.eshop.eshop.repository;
 
-import com.eshop.eshop.models.Item;
+import com.eshop.eshop.models.ItemEntity;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends PagingAndSortingRepository<Item, Long> {
+public interface ItemRepository extends PagingAndSortingRepository<ItemEntity, Long>, JpaRepository<ItemEntity, Long> {
 
-    List<Item> findByDetailsLike(String details, Pageable pageable);
+    List<ItemEntity> findByDetailsLike(String details, Pageable pageable);
 
     @Query(value = "SELECT * FROM ITEM WHERE DETAILS LIKE :details", nativeQuery = true)
-    List<Item> selByDetailsLike(@Param("details") String details);
+    List<ItemEntity> selByDetailsLike(@Param("details") String details);
 }
