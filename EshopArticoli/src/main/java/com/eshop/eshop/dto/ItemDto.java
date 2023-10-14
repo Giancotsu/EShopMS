55bdf78ce79955a140a4320dfccdcd656ea4a3ad
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-14 14:47:05 +0200

    item dto enhanced

--- EshopArticoli/src/main/java/com/eshop/eshop/dto/ItemDto.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/dto/ItemDto.java
@@ -2,11 +2,13 @@ package com.eshop.eshop.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class ItemDto {
 
     private long id;
     private String name;
     private String details;
-    private double price;
+    private BigDecimal price;
 }
