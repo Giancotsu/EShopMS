62e2025b53f1acd28f1e7c6455c9fb5a3b2b829a
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-15 11:18:44 +0200

    iva entity completed

--- EshopArticoli/src/main/java/com/eshop/eshop/models/IvaEntity.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/models/IvaEntity.java
@@ -1,4 +1,41 @@
 package com.eshop.eshop.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class IvaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long ivaId;
+    private int ivaValue;
+
+    //contructor
+
+    public IvaEntity() {}
+
+    public IvaEntity(int ivaValue) {
+        this.ivaValue = ivaValue;
+    }
+
+    //getter & setter
+
+    public long getIvaId() {
+        return ivaId;
+    }
+
+    public void setIvaId(long ivaId) {
+        this.ivaId = ivaId;
+    }
+
+    public int getIvaValue() {
+        return ivaValue;
+    }
+
+    public void setIvaValue(int ivaValue) {
+        this.ivaValue = ivaValue;
+    }
 }
