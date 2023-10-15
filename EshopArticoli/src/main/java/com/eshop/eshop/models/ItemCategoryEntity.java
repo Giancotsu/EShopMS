7d9b4c99f040861e9a4408d34605836911496035
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-15 11:34:00 +0200

    category entity completed

--- EshopArticoli/src/main/java/com/eshop/eshop/models/ItemCategoryEntity.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/models/ItemCategoryEntity.java
@@ -1,4 +1,43 @@
 package com.eshop.eshop.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "CATEGORY")
 public class ItemCategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long categoryId;
+
+    private String categoryName;
+
+    //contructor
+
+    public ItemCategoryEntity() {
+    }
+
+    public ItemCategoryEntity(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    //Getter & Setter
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
