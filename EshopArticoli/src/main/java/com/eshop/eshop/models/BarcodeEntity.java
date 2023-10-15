d2603d45c4d5409cace05ef4c552b5f9ef8f9562
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-15 11:27:18 +0200

    barcode entity completed

--- EshopArticoli/src/main/java/com/eshop/eshop/models/BarcodeEntity.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/models/BarcodeEntity.java
@@ -1,4 +1,43 @@
 package com.eshop.eshop.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "BARCODE")
 public class BarcodeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long barcodeId;
+
+    private String barcode;
+
+    //contructor
+
+    public BarcodeEntity() {
+    }
+
+    public BarcodeEntity(String barcode) {
+        this.barcode = barcode;
+    }
+
+    //getter & setter
+
+    public long getBarcodeId() {
+        return barcodeId;
+    }
+
+    public void setBarcodeId(long barcodeId) {
+        this.barcodeId = barcodeId;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
 }
