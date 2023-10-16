8efbd0be6ccddfb3d550a6ac4b921b9398b105fe
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-16 12:32:07 +0200

    item controller tests enhanced

--- EshopArticoli/src/test/java/com/eshop/eshop/controller/ItemControllerTests.java
+++ EshopArticoli/src/test/java/com/eshop/eshop/controller/ItemControllerTests.java
@@ -1,11 +1,12 @@
 package com.eshop.eshop.controller;
 
 import com.eshop.eshop.controllers.ItemController;
-import com.eshop.eshop.models.Item;
+import com.eshop.eshop.dto.ItemDto;
+import com.eshop.eshop.models.ItemEntity;
+import com.eshop.eshop.service.ItemService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,7 +17,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.BDDMockito.given;
+import java.math.BigDecimal;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(controllers = ItemController.class)
@@ -34,21 +36,20 @@ public class ItemControllerTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    private Item item;
+    private ItemEntity item;
 
     private ItemDto itemDto;
 
     @BeforeEach
     public void init(){
-        item = new Item("iphone 30", "Smartphone, 4k resolution, 50GB RAM, 10TB internal memory",
-                3900.99);
+        item = new ItemEntity("iphone 30", "Smartphone, 4k resolution, 50GB RAM, 10TB internal memory", BigDecimal.valueOf(13000.50));
     }
 
     @Test
     @Order(1)
     public void ArticoliController_CreateArticolo_ReturnCreated() throws Exception {
 
-        given(itemService.createItem(ArgumentMatchers.any())).willAnswer((invocation -> invocation.getArgument(0)));
+        //given(itemService.createItem(ArgumentMatchers.any())).willAnswer((invocation -> invocation.getArgument(0)));
 
         ResultActions response = mockMvc.perform(post("api/items/create")
                 .contentType(MediaType.APPLICATION_JSON)
