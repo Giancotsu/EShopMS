a5ad235a42cb8281a4c65c81041384af8b026707
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-16 22:53:02 +0200

    crud methods added

--- EshopArticoli/src/main/java/com/eshop/eshop/service/IvaService.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/service/IvaService.java
@@ -1,4 +1,12 @@
 package com.eshop.eshop.service;
 
+import com.eshop.eshop.dto.IvaDto;
+
+import java.util.List;
+
 public interface IvaService {
+
+    List<IvaDto> getAllIva();
+    IvaDto getIvaById(Long ivaId);
+    IvaDto createIva(IvaDto ivaDto);
 }
