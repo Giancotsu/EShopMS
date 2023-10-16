5d98715d59f924cb870d70daf386eebce348fd3c
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-16 12:30:41 +0200

    category service enhanced

--- EshopArticoli/src/main/java/com/eshop/eshop/service/CategoryService.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/service/CategoryService.java
@@ -1,4 +1,14 @@
 package com.eshop.eshop.service;
 
+import com.eshop.eshop.dto.CategoryDto;
+
+import java.util.List;
+
 public interface CategoryService {
+
+
+    List<CategoryDto> getAllCategories();
+    CategoryDto getCategoryByName(String name);
+    CategoryDto createCategory(CategoryDto categoryDto);
+
 }
