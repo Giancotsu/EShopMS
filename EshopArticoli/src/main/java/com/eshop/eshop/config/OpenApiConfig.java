36f01ff5c1a8cf79f4f16001eda0ac154ad8d015
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-17 23:55:19 +0200

    application information added

--- EshopArticoli/src/main/java/com/eshop/eshop/config/OpenApiConfig.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/config/OpenApiConfig.java
@@ -1,4 +1,29 @@
 package com.eshop.eshop.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                    name = "Gianfranco Larocca",
+                    email = "gianfrancolarocca4j@gmail.com",
+                    url = "https://github.com/Giancotsu"
+                ),
+                description = "Eshop microservices (all my knowledge)",
+                title = "OpenApi - Eshop",
+                version = "1.0",
+                license = @License(
+                        name = "license-name",
+                        url = "https://license-site.com"
+                ),
+                termsOfService = "Terms of service"
+
+        )
+)
 public class OpenApiConfig {
 }
