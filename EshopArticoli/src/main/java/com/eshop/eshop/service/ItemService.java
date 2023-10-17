295fbdb33e29573e80f0f177fb6ec4cbf2886cf2
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-17 13:58:54 +0200

    get items by category name method added

--- EshopArticoli/src/main/java/com/eshop/eshop/service/ItemService.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/service/ItemService.java
@@ -4,6 +4,8 @@ import com.eshop.eshop.dto.ItemDto;
 import com.eshop.eshop.dto.ItemResponse;
 import com.eshop.eshop.models.ItemCategoryEntity;
 
+import java.util.List;
+
 public interface ItemService {
 
     ItemResponse getAllItems(int pageNumber, int pageSize);
@@ -11,6 +13,7 @@ public interface ItemService {
     ItemDto createItem(ItemDto itemDto);
     ItemDto updateItem(ItemDto itemDto, long id);
     void deleteItemById(long id);
-
     ItemDto setItemCategory(Long itemId, ItemCategoryEntity category);
+
+    List<ItemDto> getItemsByCategory(String categoryName);
 }
