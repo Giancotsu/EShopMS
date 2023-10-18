d8aa1aa413e841dcb12baf30d4e07b181b7b7ccc
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-18 20:14:40 +0200

    fields documentation added

--- EshopArticoli/src/main/java/com/eshop/eshop/dto/ItemDto.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/dto/ItemDto.java
@@ -2,6 +2,7 @@ package com.eshop.eshop.dto;
 
 import com.eshop.eshop.models.ItemCategoryEntity;
 import com.eshop.eshop.models.IvaEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -15,13 +16,16 @@ public class ItemDto {
 
     private long id;
     @NotNull(message = "required")
-    @Size(min=3, message = "min 3")
+    @Size(min=3, max = 50, message = "min 3, max 50")
+    @Schema(name = "name", description = "Item name")
     private String name;
     @NotNull(message = "required")
-    @Size(min=5, message="min 5")
+    @Size(min=5, max = 200, message="min 5, max 200")
+    @Schema(name = "details", description = "Item details")
     private String details;
     @NotNull(message = "required")
     @Min(value = 0, message = "min zero")
+    @Schema(name = "price", description = "Item price")
     private BigDecimal price;
     private List<ItemCategoryEntity> categories;
     private IvaEntity iva;
