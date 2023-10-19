adeffa156436d49b451811cb5b562fe0bdac530d
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-19 21:52:25 +0200

    iva dto enhanced

--- EshopArticoli/src/main/java/com/eshop/eshop/dto/IvaDto.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/dto/IvaDto.java
@@ -2,8 +2,14 @@ package com.eshop.eshop.dto;
 
 import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
-public class IvaDto {
+public class IvaDto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 5300033926473664629L;
 
     private int ivaValue;
 }
