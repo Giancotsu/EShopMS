879b89e702050d50a145a642e87b74762901a64d
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-21 12:21:50 +0200

    items count info added

--- EshopArticoli/src/main/java/com/eshop/eshop/components/ActuatorCustomEndpoint.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/components/ActuatorCustomEndpoint.java
@@ -3,10 +3,11 @@ package com.eshop.eshop.components;
 import com.eshop.eshop.repository.ItemRepository;
 import lombok.Data;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.stereotype.Component;
 
 @Component
-@Endpoint(id = "")
+@Endpoint(id = "itemsInfo")
 @Data
 public class ActuatorCustomEndpoint {
 
@@ -14,5 +15,10 @@ public class ActuatorCustomEndpoint {
 
     public ActuatorCustomEndpoint(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
+    }
+
+    @ReadOperation
+    public String itemsInfo(){
+        return String.format("Quantity of items: %d", itemRepository.itemsCount());
     }
 }
