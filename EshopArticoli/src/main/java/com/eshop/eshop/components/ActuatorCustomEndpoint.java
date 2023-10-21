b4cef1773828db1546746d5c75abe3d8f3091880
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-21 12:37:47 +0200

    get items or item endpoint added

--- EshopArticoli/src/main/java/com/eshop/eshop/components/ActuatorCustomEndpoint.java
+++ EshopArticoli/src/main/java/com/eshop/eshop/components/ActuatorCustomEndpoint.java
@@ -1,10 +1,17 @@
 package com.eshop.eshop.components;
 
+import com.eshop.eshop.models.ItemEntity;
 import com.eshop.eshop.repository.ItemRepository;
 import lombok.Data;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @Endpoint(id = "itemsInfo")
@@ -20,5 +27,22 @@ public class ActuatorCustomEndpoint {
     @ReadOperation
     public String itemsInfo(){
         return String.format("Quantity of items: %d", itemRepository.itemsCount());
+    }
+
+    @ReadOperation
+    public List<ItemEntity> itemsInfo(@Selector String name){
+        if(name.equalsIgnoreCase("all")){
+            return itemRepository.findAll();
+        }else{
+            List<ItemEntity> items = new ArrayList<>();
+            try{
+                long id = Long.parseLong(name);
+                Optional<ItemEntity> optionalItem= itemRepository.findById(id);
+                optionalItem.ifPresent(items::add);
+            }catch (NumberFormatException exception){
+                System.out.println(Arrays.toString(exception.getStackTrace()));
+            }
+            return items;
+        }
     }
 }
