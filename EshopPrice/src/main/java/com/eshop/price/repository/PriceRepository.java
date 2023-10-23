42e7fa70c19a7895de3a8767ae5a4e9f0054cca9
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-23 20:39:29 +0200

    useless classes removed

--- EshopPrice/src/main/java/com/eshop/price/repository/PriceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eshop.price.repository;
-
-import com.eshop.price.entities.PriceListDetailsEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PriceRepository extends JpaRepository<PriceListDetailsEntity, Long> {
-}
