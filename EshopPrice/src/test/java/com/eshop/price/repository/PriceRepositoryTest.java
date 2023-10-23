61126f2bf5582eb0e0297865665fb5dd651d1552
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-23 19:24:08 +0200

    first test added

--- EshopPrice/src/test/java/com/eshop/price/repository/PriceRepositoryTest.java
+++ EshopPrice/src/test/java/com/eshop/price/repository/PriceRepositoryTest.java
@@ -1,5 +1,9 @@
 package com.eshop.price.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.eshop.price.entities.PriceListDetailsEntity;
+import com.eshop.price.entities.PriceListEntity;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -7,40 +11,41 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.math.BigDecimal;
 import java.util.HashSet;
+import java.util.Set;
 
 @TestPropertySource(locations="classpath:application-price-list.properties")
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PriceRepositoryTest {
 
-    private final PriceRepository priceRepository;
+    private final PriceListRepository priceListRepository;
 
-    public PriceRepositoryTest(PriceRepository priceRepository) {
-        this.priceRepository = priceRepository;
+    long priceListId = 100;
+    long itemId = 1;
+    BigDecimal price = BigDecimal.valueOf(1.00);
+
+    public PriceRepositoryTest(PriceListRepository priceListRepository) {
+        this.priceListRepository = priceListRepository;
     }
-
-    String priceListId = "100";
-    String itemId = "1";
-    Double price = 1.00;
 
     @Test
     @Order(1)
     public void testInsListino()
     {
-        PriceList priceListTest = new PriceList(priceListId,"Price list test 100","No");
+        PriceListEntity priceListTest = new PriceListEntity(priceListId, "Price list test 100",false);
 
-        Set<PriceListDetails> listDetails = new HashSet<>();
-        PriceListDetails listDetailsTest = new PriceListDetails(itemId, price, priceListTest);
+        Set<PriceListDetailsEntity> listDetails = new HashSet<>();
+        PriceListDetailsEntity listDetailsTest = new PriceListDetailsEntity(itemId, price);
+        listDetailsTest.setPriceList(priceListTest);
         listDetails.add(listDetailsTest);
 
         priceListTest.setPriceListDetails(listDetails);
 
-        priceRepository.save(priceListTest);
+        PriceListEntity priceList = priceListRepository.save(priceListTest);
 
-        assertThat(priceRepository
-                .findById(PriceListId))
-                .isNotEmpty();
+        assertThat(priceListRepository.findById(priceList.getPriceListId())).isNotEmpty();
 
     }
 }
