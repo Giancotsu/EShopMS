e13b172dd6b55978de71f9a8da5dad019cf6d1e8
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-23 15:50:44 +0200

    price list details entity enhanced

--- EshopPrice/src/main/java/com/eshop/price/entities/PriceListDetails.java
+++ EshopPrice/src/main/java/com/eshop/price/entities/PriceListDetails.java
@@ -1,4 +1,52 @@
 package com.eshop.price.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity(name = "PRICE-LIST-DETAILS")
 public class PriceListDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long priceListDetailsId;
+
+    private long itemId;
+
+    private BigDecimal price;
+
+    public PriceListDetails() {
+    }
+
+    public PriceListDetails(long itemId, BigDecimal price) {
+        this.itemId = itemId;
+        this.price = price;
+    }
+
+    public long getPriceListDetailsId() {
+        return priceListDetailsId;
+    }
+
+    public void setPriceListDetailsId(long priceListDetailsId) {
+        this.priceListDetailsId = priceListDetailsId;
+    }
+
+    public long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(long itemId) {
+        this.itemId = itemId;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
