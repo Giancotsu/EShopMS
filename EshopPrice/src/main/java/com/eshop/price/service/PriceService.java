94eab908c9e521afa06ec207a63176b76a65a2cd
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-23 22:19:16 +0200

    price service enhanced

--- EshopPrice/src/main/java/com/eshop/price/service/PriceService.java
+++ EshopPrice/src/main/java/com/eshop/price/service/PriceService.java
@@ -1,4 +1,9 @@
 package com.eshop.price.service;
 
+import com.eshop.price.dtos.PriceDto;
+
 public interface PriceService {
+
+    PriceDto getPriceByItem(long itemId);
+    PriceDto setPriceToItem(PriceDto price, long itemId);
 }
