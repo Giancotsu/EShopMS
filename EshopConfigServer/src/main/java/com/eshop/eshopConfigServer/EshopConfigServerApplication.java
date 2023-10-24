a6b37d370139cb6e1cca71b2ad091a05b63d211f
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-24 12:22:15 +0200

    config server updated

---
+++ EshopConfigServer/src/main/java/com/eshop/eshopConfigServer/EshopConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.eshopConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopConfigServerApplication.class, args);
+	}
+
+}
