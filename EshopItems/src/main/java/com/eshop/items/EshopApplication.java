8d09d4648d4548d7e0e975efe54a4a7ddeda5fea
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-26 20:06:48 +0200

    openfeign enabled

--- EshopItems/src/main/java/com/eshop/items/EshopApplication.java
+++ EshopItems/src/main/java/com/eshop/items/EshopApplication.java
@@ -2,10 +2,12 @@ package com.eshop.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
+@EnableFeignClients
 public class EshopApplication {
 
 	public static void main(String[] args) {
