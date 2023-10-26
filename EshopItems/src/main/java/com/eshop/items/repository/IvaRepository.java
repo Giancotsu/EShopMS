f9f41c78079327ffa307d014afebce85e722ae16
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-26 20:26:48 +0200

    iva removed

--- EshopItems/src/main/java/com/eshop/items/repository/IvaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eshop.items.repository;
-
-import com.eshop.items.entities.IvaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IvaRepository extends JpaRepository<IvaEntity, Long> {
-}
