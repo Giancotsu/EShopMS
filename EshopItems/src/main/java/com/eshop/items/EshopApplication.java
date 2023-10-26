d10afd99780a8abfc64261867b44fa1d1cee67b6
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-26 20:08:05 +0200

    openfeign enabled

--- EshopItems/src/main/java/com/eshop/items/EshopApplication.java
+++ EshopItems/src/main/java/com/eshop/items/EshopApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
 @EnableFeignClients
-
 public class EshopApplication {
 
 	public static void main(String[] args) {
