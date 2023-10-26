93e080dc7467f49fc20f574cc0b9f2b0bad1b9bd
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-26 20:31:30 +0200

    barcode removed

--- EshopItems/src/main/java/com/eshop/items/repository/BarcodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eshop.items.repository;
-
-import com.eshop.items.entities.BarcodeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BarcodeRepository extends JpaRepository<BarcodeEntity, Long> {
-}
