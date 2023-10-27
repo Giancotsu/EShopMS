9d08027503b53f014669851fa8be78eb0b5a7fc4
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-27 14:03:15 +0200

    set item price implemented

--- EshopPrice/src/main/java/com/eshop/price/service/impl/PriceServiceImpl.java
+++ EshopPrice/src/main/java/com/eshop/price/service/impl/PriceServiceImpl.java
@@ -1,12 +1,14 @@
 package com.eshop.price.service.impl;
 
 import com.eshop.price.dtos.PriceDto;
+import com.eshop.price.dtos.mapper.PriceMapper;
 import com.eshop.price.entities.PriceEntity;
 import com.eshop.price.repositories.PriceRepository;
 import com.eshop.price.service.PriceService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 
 @Service
 public class PriceServiceImpl implements PriceService {
@@ -27,7 +29,19 @@ public class PriceServiceImpl implements PriceService {
     }
 
     @Override
-    public PriceDto setPriceToItem(PriceDto price, long itemId) {
-        return null;
+    public PriceDto setPriceToItem(long itemId, BigDecimal price) {
+        PriceDto priceDto;
+        PriceEntity priceEntity = priceRepository.findPriceEntityByItemId(itemId);
+        if(priceEntity!=null){
+            priceDto = PriceMapper.entityToDto(priceEntity);
+            priceDto.setPrice(price);
+        }else{
+            priceDto = new PriceDto();
+            priceDto.setItemId(itemId);
+            priceDto.setPrice(price);
+            priceDto.setSales(new HashSet<>());
+        }
+
+        return PriceMapper.entityToDto(priceRepository.save(PriceMapper.dtoToEntity(priceDto)));
     }
 }
