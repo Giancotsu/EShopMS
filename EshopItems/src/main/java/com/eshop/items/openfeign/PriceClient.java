34852a855f738451633f69e52ddad48c81532b36
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-27 19:44:12 +0200

    set item price implemented

--- EshopItems/src/main/java/com/eshop/items/openfeign/PriceClient.java
+++ EshopItems/src/main/java/com/eshop/items/openfeign/PriceClient.java
@@ -2,8 +2,11 @@ package com.eshop.items.openfeign;
 
 import com.eshop.items.dto.ItemDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.math.BigDecimal;
 
@@ -12,4 +15,7 @@ public interface PriceClient {
 
     @GetMapping(value = "/api/price/item/{itemId}")
     public BigDecimal getItemPrice(@PathVariable("itemId") long itemId);
+
+    @PostMapping(value = "/api/price/set/{itemId}")
+    public void setItemPrice(@PathVariable("itemId") long itemId, @RequestBody BigDecimal price);
 }
