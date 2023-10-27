6aa894d48f74226d0fd9c761ce4d5d5298955c69
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-27 19:01:28 +0200

    openfeign item client added

---
+++ EshopPrice/src/main/java/com/eshop/price/openfeign/ItemClient.java
@@ -0,0 +1,15 @@
+package com.eshop.price.openfeign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "EshopItems", url = "localhost:9000")
+public interface ItemClient {
+
+    @GetMapping(value = "/api/item/clear-cache/{itemId}")
+    ResponseEntity<String> clearItemsCache(@PathVariable("itemId") long itemId);
+}
