50f62b2ed639a69d0e3a7f1a033472575e8c4d30
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-28 19:40:05 +0200

    exception added

--- EshopItems/src/main/java/com/eshop/items/exceptions/GlobalExceptionHandler.java
+++ EshopItems/src/main/java/com/eshop/items/exceptions/GlobalExceptionHandler.java
@@ -2,13 +2,13 @@ package com.eshop.items.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ItemNotFoundException.class)
@@ -16,10 +16,36 @@ public class GlobalExceptionHandler {
 
         ErrorObj errorObj = new ErrorObj();
 
-        errorObj.setStatusCode(HttpStatus.NOT_FOUND.value());
+        errorObj.setCode(HttpStatus.NOT_FOUND.value());
+        errorObj.setStatus(HttpStatus.NOT_FOUND.name());
         errorObj.setMessage(exception.getMessage());
+        errorObj.setDetails(request.getDescription(false));
         errorObj.setTimestamp(new Date());
 
         return new ResponseEntity<>(errorObj, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({PriceServiceNotAvailableException.class})
+    public ResponseEntity<ErrorObj> handleProductServiceNotAvailableException(PriceServiceNotAvailableException exception, WebRequest request) {
+
+        return new ResponseEntity<>(new ErrorObj(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage(), request.getDescription(false)), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({PriceNotFoundException.class})
+    public ResponseEntity<ErrorObj> handleProductNotFoundException(PriceNotFoundException exception, WebRequest request) {
+        return new ResponseEntity<>(new ErrorObj(
+                HttpStatus.NOT_FOUND,
+                exception.getMessage(),
+                request.getDescription(false)),
+                HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorObj> handleGenericException(Exception exception, WebRequest request) {
+        return new ResponseEntity<>(new ErrorObj(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                request.getDescription(false)),
+                HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
