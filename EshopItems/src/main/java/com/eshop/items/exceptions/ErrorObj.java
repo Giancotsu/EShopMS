8fc1e7c42ddd1530c9023a8d6e4cde246e7fd65c
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-28 19:41:43 +0200

    error object enhanced

--- EshopItems/src/main/java/com/eshop/items/exceptions/ErrorObj.java
+++ EshopItems/src/main/java/com/eshop/items/exceptions/ErrorObj.java
@@ -4,9 +4,36 @@ import lombok.Data;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.http.HttpStatus;
+
 @Data
 public class ErrorObj {
-    private Integer statusCode;
+
+    @JsonProperty(value = "code")
+    private int code;
+
+    @JsonProperty(value = "status")
+    private String status;
+
+    @JsonProperty(value = "message")
     private String message;
+
+    @JsonProperty(value = "details")
+    private String details;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private Date timestamp;
+
+    public ErrorObj() {
+    }
+
+    public ErrorObj(HttpStatus httpStatus, String message, String details) {
+        this.code = httpStatus.value();
+        this.status = httpStatus.name();
+        this.message = message;
+        this.details = details;
+        this.timestamp = new Date();
+    }
 }
