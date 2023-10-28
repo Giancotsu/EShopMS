00984ed5f1652fda4014a59c33f0d61b0e59c8cc
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-28 20:52:20 +0200

    enhanced

--- EshopItems/src/main/java/com/eshop/items/openfeign/CustomErrorDecoder.java
+++ EshopItems/src/main/java/com/eshop/items/openfeign/CustomErrorDecoder.java
@@ -5,14 +5,17 @@ import com.eshop.items.exceptions.PriceNotFoundException;
 import com.eshop.items.exceptions.PriceServiceNotAvailableException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomErrorDecoder implements ErrorDecoder {
     @Override
     public Exception decode(String methodKey, Response response) {
+        System.err.println(response.status());
         return switch (response.status()) {
             case 400 -> new BadRequestException();
             case 404 -> new PriceNotFoundException("Price not found");
-            case 503 -> new PriceServiceNotAvailableException("Price Api is unavailable");
+            case 500 -> new PriceServiceNotAvailableException("Price Api is unavailable");
             default -> new Exception("Exception while getting price");
         };
     }
