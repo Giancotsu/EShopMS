f6e4385a12f1d70fe7276af02c292ed38f3fe2f2
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-28 20:52:30 +0200

    enhanced

--- EshopItems/src/main/java/com/eshop/items/openfeign/OpenFeignConfig.java
+++ EshopItems/src/main/java/com/eshop/items/openfeign/OpenFeignConfig.java
@@ -2,9 +2,7 @@ package com.eshop.items.openfeign;
 
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class OpenFeignConfig {
 
     @Bean
