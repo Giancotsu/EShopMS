dba9520632246a8e2cae37d8af17c9e37a065778
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-29 12:31:51 +0100

    price client enhanced

--- EshopItems/src/main/java/com/eshop/items/openfeign/PriceClient.java
+++ EshopItems/src/main/java/com/eshop/items/openfeign/PriceClient.java
@@ -1,8 +1,8 @@
 package com.eshop.items.openfeign;
 
-import com.eshop.items.dto.ItemDto;
+
+import com.eshop.items.config.OpenFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
