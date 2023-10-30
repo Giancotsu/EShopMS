95af97b2f2dceffdc503d5c8da312c6935f2353f
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-30 13:03:22 +0100

    set price and item categories ids added

--- EshopPrice/src/main/java/com/eshop/price/service/PriceService.java
+++ EshopPrice/src/main/java/com/eshop/price/service/PriceService.java
@@ -1,5 +1,6 @@
 package com.eshop.price.service;
 
+import com.eshop.price.dtos.ItemClientRequestPriceCategory;
 import com.eshop.price.dtos.PriceDto;
 
 import java.math.BigDecimal;
@@ -8,4 +9,5 @@ public interface PriceService {
 
     BigDecimal getPriceByItem(long itemId);
     PriceDto setPriceToItem(long itemId, BigDecimal price);
+    PriceDto setItemPriceAndCategories(ItemClientRequestPriceCategory itemClientRequest);
 }
