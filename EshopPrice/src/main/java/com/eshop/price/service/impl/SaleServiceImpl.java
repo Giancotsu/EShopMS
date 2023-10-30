5b4c0d93cbf36db5b92279b2b80e4602d13757b8
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-30 23:28:24 +0100

    create sale implemented

---
+++ EshopPrice/src/main/java/com/eshop/price/service/impl/SaleServiceImpl.java
@@ -0,0 +1,23 @@
+package com.eshop.price.service.impl;
+
+import com.eshop.price.dtos.SaleDto;
+import com.eshop.price.dtos.mapper.SaleMapper;
+import com.eshop.price.repositories.SaleRepository;
+import com.eshop.price.service.SaleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SaleServiceImpl implements SaleService {
+
+    private final SaleRepository saleRepository;
+
+    public SaleServiceImpl(SaleRepository saleRepository) {
+        this.saleRepository = saleRepository;
+    }
+
+
+    @Override
+    public SaleDto createSale(SaleDto saleDto) {
+        return SaleMapper.entityToDto(saleRepository.save(SaleMapper.dtoToEntity(saleDto)));
+    }
+}
