c51f3310c40fb715995e7e85e9fc3d60b3da4bb5
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-30 21:24:16 +0100

    enhanced

--- EshopItems/src/main/java/com/eshop/items/openfeign/PriceClient.java
+++ EshopItems/src/main/java/com/eshop/items/openfeign/PriceClient.java
@@ -4,14 +4,12 @@ package com.eshop.items.openfeign;
 import com.eshop.items.config.OpenFeignConfig;
 import com.eshop.items.dto.SetPriceCategoriesDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.math.BigDecimal;
-import java.util.Set;
 
 @FeignClient(name = "EshopPrice", url = "localhost:9001", configuration = {OpenFeignConfig.class})
 public interface PriceClient {
