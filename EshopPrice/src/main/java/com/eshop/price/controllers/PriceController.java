28d2866e1305bad215eae6026f7b65933e065d3c
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-30 13:01:57 +0100

    set item price and categories ids added

--- EshopPrice/src/main/java/com/eshop/price/controllers/PriceController.java
+++ EshopPrice/src/main/java/com/eshop/price/controllers/PriceController.java
@@ -1,17 +1,13 @@
 package com.eshop.price.controllers;
 
+import com.eshop.price.dtos.ItemClientRequestPriceCategory;
 import com.eshop.price.dtos.PriceDto;
 import com.eshop.price.service.impl.PriceServiceImpl;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/api/price")
@@ -33,15 +29,8 @@ public class PriceController {
         return new ResponseEntity<>(priceService.setPriceToItem(itemId, price), HttpStatus.OK);
     }
 
-    @PostMapping(value = "/set/enhanced/{itemId}")
-    public ResponseEntity<PriceDto> setItemPriceAndCategories(@PathVariable("itemId") long itemId, @RequestBody Map<String, Set<String>> requestBody){
-
-        BigDecimal price = BigDecimal.valueOf(Double.parseDouble(requestBody.get("price").stream().findFirst().get()));
-        Set<Long> itemCategories = requestBody.get("categories").stream().map(Long::parseLong).collect(Collectors.toSet());
-
-        System.out.println(price);
-        System.out.println(itemCategories);
-
-        return new ResponseEntity<>(priceService.setPriceToItem(itemId, price), HttpStatus.OK);
+    @PostMapping(value = "/set/enhanced")
+    public ResponseEntity<PriceDto> setItemPriceAndCategories(@RequestBody ItemClientRequestPriceCategory requestBody){
+        return new ResponseEntity<>(priceService.setItemPriceAndCategories(requestBody), HttpStatus.OK);
     }
 }
