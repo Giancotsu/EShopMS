bcb82990cd80957e378b0a32c23c4f4678e200db
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-10-31 12:15:27 +0100

    set price sale single added

--- EshopPrice/src/main/java/com/eshop/price/service/PriceService.java
+++ EshopPrice/src/main/java/com/eshop/price/service/PriceService.java
@@ -2,12 +2,15 @@ package com.eshop.price.service;
 
 import com.eshop.price.dtos.ItemClientRequestPriceCategory;
 import com.eshop.price.dtos.PriceDto;
+import com.eshop.price.dtos.SaleDto;
 
 import java.math.BigDecimal;
 
 public interface PriceService {
 
+    PriceDto getPriceById(long priceId);
     BigDecimal getPriceByItem(long itemId);
     PriceDto setPriceToItem(long itemId, BigDecimal price);
     PriceDto setItemPriceAndCategories(ItemClientRequestPriceCategory itemClientRequest);
+    PriceDto setPriceSaleSingle(long priceId, SaleDto saleDto);
 }
