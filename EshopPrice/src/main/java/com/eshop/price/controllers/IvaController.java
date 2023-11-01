c53ad4aedab140cadf9886e16c9f01fa8480beca
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-01 23:51:27 +0100

    iva controller enhanced

--- EshopPrice/src/main/java/com/eshop/price/controllers/IvaController.java
+++ EshopPrice/src/main/java/com/eshop/price/controllers/IvaController.java
@@ -1,9 +1,5 @@
 package com.eshop.price.controllers;
 
-import com.eshop.price.configs.ApplicationConfigs;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
