cb14bc0805243c018a771dee905714142d022044
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-01 23:55:53 +0100

    iva repository added

---
+++ EshopPrice/src/main/java/com/eshop/price/repositories/IvaRepository.java
@@ -0,0 +1,7 @@
+package com.eshop.price.repositories;
+
+import com.eshop.price.entities.IvaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IvaRepository extends JpaRepository<IvaEntity, Long> {
+}
