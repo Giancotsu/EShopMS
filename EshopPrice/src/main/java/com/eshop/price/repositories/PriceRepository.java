293be8a1bf4d253d469ed5443807ecd456196aef
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-01 19:59:08 +0100

    enhanced

--- EshopPrice/src/main/java/com/eshop/price/repositories/PriceRepository.java
+++ EshopPrice/src/main/java/com/eshop/price/repositories/PriceRepository.java
@@ -3,7 +3,9 @@ package com.eshop.price.repositories;
 import com.eshop.price.entities.PriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PriceRepository extends JpaRepository<PriceEntity, Long> {
 
-    PriceEntity findPriceEntityByItemId(long itemId);
+    Optional<PriceEntity> findPriceEntityByItemId(long itemId);
 }
