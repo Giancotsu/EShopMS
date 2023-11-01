36d44d4f7f82abbfca83a5fed9e5e2ad29883b5e
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-01 23:53:47 +0100

    iva entity enhanced

--- EshopPrice/src/main/java/com/eshop/price/entities/IvaEntity.java
+++ EshopPrice/src/main/java/com/eshop/price/entities/IvaEntity.java
@@ -5,4 +5,29 @@ import jakarta.persistence.Entity;
 @Entity(name = "IVA")
 public class IvaEntity {
 
+    private long ivaId;
+    private int value;
+
+    public IvaEntity() {
+    }
+
+    public IvaEntity(int value) {
+        this.value = value;
+    }
+
+    public long getIvaId() {
+        return ivaId;
+    }
+
+    public void setIvaId(long ivaId) {
+        this.ivaId = ivaId;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
