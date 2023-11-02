3f7898dfcfabbe29de0b0249c75b04d6cbcf5825
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-02 17:25:29 +0100

    enhanced

--- EshopPrice/src/main/java/com/eshop/price/repositories/IvaRepository.java
+++ EshopPrice/src/main/java/com/eshop/price/repositories/IvaRepository.java
@@ -2,6 +2,12 @@ package com.eshop.price.repositories;
 
 import com.eshop.price.entities.IvaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface IvaRepository extends JpaRepository<IvaEntity, Long> {
+
+    @Query(value = "SELECT * FROM IVA WHERE IVA.VALUE = :value", nativeQuery = true)
+    Optional<IvaEntity> selIvaByValue(int value);
 }
