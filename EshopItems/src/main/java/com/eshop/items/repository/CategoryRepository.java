315d28fb93d8f64e387babc211a970934fd9741e
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-03 22:06:20 +0100

    find by category name added

--- EshopItems/src/main/java/com/eshop/items/repository/CategoryRepository.java
+++ EshopItems/src/main/java/com/eshop/items/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.eshop.items.repository;
 import com.eshop.items.entities.ItemCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<ItemCategoryEntity, Long> {
+    Optional<ItemCategoryEntity> findByCategoryName(String name);
 }
