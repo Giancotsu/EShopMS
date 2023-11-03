2add9cbd74d608c3389da7d03c542dca5a440d39
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-03 22:05:44 +0100

    category not found exception added

---
+++ EshopItems/src/main/java/com/eshop/items/exceptions/CategoryNotFoundException.java
@@ -0,0 +1,8 @@
+package com.eshop.items.exceptions;
+
+public class CategoryNotFoundException extends RuntimeException{
+
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+}
