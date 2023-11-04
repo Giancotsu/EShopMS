9cbb4c71529f0d00cbe285677fd2edcbf4965c41
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-04 22:20:30 +0100

    set price sale by category added

--- EshopPrice/src/main/java/com/eshop/price/controllers/PriceController.java
+++ EshopPrice/src/main/java/com/eshop/price/controllers/PriceController.java
@@ -3,12 +3,14 @@ package com.eshop.price.controllers;
 import com.eshop.price.dtos.ItemClientRequestPriceCategory;
 import com.eshop.price.dtos.PriceDto;
 import com.eshop.price.dtos.SaleDto;
+import com.eshop.price.entities.PriceEntity;
 import com.eshop.price.service.impl.PriceServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/price")
@@ -58,5 +60,10 @@ public class PriceController {
     @DeleteMapping(value = "/{itemId}/remove/sale/{saleId}")
     public ResponseEntity<String> removeSaleFromSinglePrice(@PathVariable("itemId") long itemId, @PathVariable("saleId") long saleId){
         return new ResponseEntity<>(priceService.removeSaleFromSinglePrice(itemId, saleId), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/sale/{saleId}/category/{categoryId}")
+    public ResponseEntity<List<PriceDto>> setPriceSaleByCategory(@PathVariable("saleId") long saleId, @PathVariable("categoryId") long categoryId){
+        return new ResponseEntity<>(priceService.setPriceSaleByCategory(saleId, categoryId), HttpStatus.OK);
     }
 }
