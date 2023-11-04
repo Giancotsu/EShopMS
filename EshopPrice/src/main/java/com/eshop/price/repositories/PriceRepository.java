9d1669dd31090e52e6aae4ae8d921a4ee57ddb55
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-04 21:47:13 +0100

    find price by category added

--- EshopPrice/src/main/java/com/eshop/price/repositories/PriceRepository.java
+++ EshopPrice/src/main/java/com/eshop/price/repositories/PriceRepository.java
@@ -2,10 +2,15 @@ package com.eshop.price.repositories;
 
 import com.eshop.price.entities.PriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PriceRepository extends JpaRepository<PriceEntity, Long> {
 
     Optional<PriceEntity> findPriceEntityByItemId(long itemId);
+
+    @Query(value = "SELECT * FROM PRICE WHERE :categoryId = ANY(PRICE.ITEM_CATEGORIES_ID)", nativeQuery = true)
+    Optional<List<PriceEntity>> findPriceByCategory(long categoryId);
 }
