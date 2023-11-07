eeed29014174635e33cba806bb37e75938064a6c
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-07 13:19:01 +0100

    enhanced

--- EshopPrice/src/main/java/com/eshop/price/configs/ApplicationConfigs.java
+++ EshopPrice/src/main/java/com/eshop/price/configs/ApplicationConfigs.java
@@ -1,10 +1,11 @@
 package com.eshop.price.configs;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-//@ConfigurationProperties("application")
+@ConfigurationProperties("application")
 public class ApplicationConfigs {
 
     //@Value("${x}")
