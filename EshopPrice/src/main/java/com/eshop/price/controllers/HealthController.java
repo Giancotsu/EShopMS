ba0d37c53787e0574bf5ab509f5a94af439e7794
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-07 13:19:09 +0100

    enhanced

--- EshopPrice/src/main/java/com/eshop/price/controllers/HealthController.java
+++ EshopPrice/src/main/java/com/eshop/price/controllers/HealthController.java
@@ -1,5 +1,8 @@
 package com.eshop.price.controllers;
 
+import com.eshop.price.configs.ApplicationConfigs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/price-service")
 public class HealthController {
 
+    @Autowired
+    private ApplicationConfigs applicationConfigs;
+
     @GetMapping(value = "is-on")
     public ResponseEntity<Boolean> amIOn(){
         return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "x")
+    public ResponseEntity<Integer> getXValue(){
+        return new ResponseEntity<>(applicationConfigs.getX(), HttpStatus.OK);
     }
 }
