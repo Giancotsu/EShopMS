b2376b0fc12b0f9ef1df230f7d11898da30b6db7
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-18 20:14:19 +0100

    test method removed

--- EshopPrice/src/main/java/com/eshop/price/controllers/HealthController.java
+++ EshopPrice/src/main/java/com/eshop/price/controllers/HealthController.java
@@ -1,8 +1,5 @@
 package com.eshop.price.controllers;
 
-import com.eshop.price.configs.ApplicationConfigs;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/price-service")
 public class HealthController {
-
-    @Autowired
-    private ApplicationConfigs applicationConfigs;
 
     @GetMapping(value = "is-on")
     public ResponseEntity<Boolean> amIOn(){
