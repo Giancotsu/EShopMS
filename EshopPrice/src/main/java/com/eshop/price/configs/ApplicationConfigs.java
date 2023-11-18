7f3f903125b95ec499111164b2afffca994777d9
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-18 20:14:07 +0100

    test method removed

--- EshopPrice/src/main/java/com/eshop/price/configs/ApplicationConfigs.java
+++ EshopPrice/src/main/java/com/eshop/price/configs/ApplicationConfigs.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
-@ConfigurationProperties("application")
+//@Component
+//@ConfigurationProperties("application")
 public class ApplicationConfigs {
 
     //@Value("${x}")
