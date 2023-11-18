80576a838498f068aefeaf52afffd0434c7eebcb
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2023-11-18 20:15:57 +0100

    discovery client enabled

--- EshopItems/src/main/java/com/eshop/items/EshopApplication.java
+++ EshopItems/src/main/java/com/eshop/items/EshopApplication.java
@@ -2,11 +2,13 @@ package com.eshop.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
+@EnableDiscoveryClient
 @EnableFeignClients
 public class EshopApplication {
 
